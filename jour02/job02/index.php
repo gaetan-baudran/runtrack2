df4ce3b5b0dc6b29ad31d96a49a23d2c78851288
gaetan-baudran <gaetan.baudran@laplateforme.io>
2023-01-10 14:57:01 +0100

    Jour2 conditions

---
+++ jour02/job02/index.php
@@ -0,0 +1,25 @@
+<!DOCTYPE html>
+<html lang="en">
+<head>
+    <meta charset="UTF-8">
+    <meta http-equiv="X-UA-Compatible" content="IE=edge">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <title>Document</title>
+</head>
+<body>
+    
+<?php
+
+   
+   for($a = 0; $a < 1338; $a++){
+ 
+           
+    if ($a != 26 && $a != 37 && $a != 88 && $a != 1111) {
+        echo $a .'<br>';
+    }
+
+   }
+
+    ?>
+</body>
+</html>
