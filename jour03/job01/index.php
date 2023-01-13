1dd1b29d3cb14c6511fa8c428f37e9a3947789df
gaetan-baudran <gaetan.baudran@laplateforme.io>
2023-01-13 12:00:53 +0100

    ajout des comm

--- jour03/job01/index.php
+++ jour03/job01/index.php
@@ -1,14 +1,16 @@
 <?php
 
-    $x = array (200,204,173,98,171,404,459);
-            foreach($x as $key => $x) {
-            if ($x % 2 == 0){
-                echo "$x est pair <br>";
-            }
-            else {
-                echo "$x est impair <br>";}
+// Notre variable '$x' est = à notre tableau 'array'. 
+$x = array(200, 204, 173, 98, 171, 404, 459);
+// foreach (pour chaque): Pour chaque variable '$x' , les variables clés '$key' ( ex: 200 , 204 ect ...)
+foreach ($x as $key => $x) {
+    // if (si): Si notre variable '$x' et un modulo de 2 ('$x % 2 ') est égale à 0 (' == ').
+    if ($x % 2 == 0) {
+        // echo (affiche) : affiche les clés 'Pair de notre tableau , de la variable $x'
+        echo "$x est pair <br>";
     }
-
-
-               
-            
+    // else (sinon): affiche les clés 'Impair de notre tableau , de la variable $x'
+    else {
+        echo "$x est impair <br>";
+    }
+}
--- jour03/job02/index.php
+++ jour03/job02/index.php
@@ -1,14 +1,17 @@
 <?php
 
 
-    
-    $str = "Tous ces instants seront perdus dans le temps comme les larmes sous la pluie.";
-    
-    for ($i = 0; isset($str[$i]);$i++){
-        if ($i % 2 == 0) {
-            echo $str[$i];
-        }
-    }
-      
+// '$str = "Tous ces instants seront perdus dans le temps comme les larmes sous la pluie." est notre varibale.
+$str = "Tous ces instants seront perdus dans le temps comme les larmes sous la pluie.";
 
-?>
+// for (pour): pour notre variable '$i' qui est égale à 0 , On isset (Determine si notre variable est déclarer et différent de null) la variable '$str' pour clé '$i' ("Tous ces instants....") en augmentant (acrémenter) de 1 '($i++)'
+for ($i = 0; isset($str[$i]); $i++) {
+
+    // if(si): Si notre variable clé '$i' est modulo de 2 (divisible par lui même) , qui égale 0 ($i % 2 == 0).
+    if ($i % 2 == 0) {
+        // echo (affiche):: affiche la variable '$str' avec la clé '$i'  
+        echo $str[$i];
+    }
+}
+      
+// Le résultat de cette algo devrait nous donner ' une lettre sur 2 ' dans notre variable .
